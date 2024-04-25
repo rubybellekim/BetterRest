a5071fb7165fd510e0c5720d2c5f5cc32205c191
rubythebelle <rubybellekim@gmail.com>
2024-04-24 20:37:14 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ruby Kim on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
